c3377bb800a63da3560db31078a3bd84d1cf902f
sujeet kushwaha <sujitkumar84143@gmail.com>
2025-09-29 15:47:15 +0530

    add project file

---
+++ init/index.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+};
+
+const initDB = async () => {
+
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+     owner: "6891f5fe6b777135bde09abe" }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
+  // console.log(initData.data);
+
+
+};
+
+initDB();
